6ab8e780e884349bf72967f200be769bfaebecc4
goshaaaa777 <isaiasddx322z@gmail.com>
2024-06-18 22:54:41 -0500

    agregacion de comentarios de apirest

--- src/main/java/pe/edu/cibertec/epi_rest_fundamentals/controller/OperacionesController.java
+++ src/main/java/pe/edu/cibertec/epi_rest_fundamentals/controller/OperacionesController.java
@@ -1,7 +1,10 @@
 package pe.edu.cibertec.epi_rest_fundamentals.controller;
 
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import pe.edu.cibertec.epi_rest_fundamentals.model.GeneralResponse;
 import pe.edu.cibertec.epi_rest_fundamentals.service.IOperacionesService;
 
 import java.util.List;
@@ -13,8 +16,13 @@ public class OperacionesController {
 
     private IOperacionesService iOperacionesService;
 @GetMapping("/esprimo/{numero}")
-    public boolean validarNumeroPrimo(@PathVariable int numero){
-    return iOperacionesService.validarNumeroEsPrimo(numero);
+    public ResponseEntity<GeneralResponse>
+    validarNumeroPrimo(@PathVariable int numero){
+    boolean resultado = iOperacionesService.validarNumeroEsPrimo(numero);
+    String mensaje = resultado?numero + " es primo" :numero + "No es primo";
+    GeneralResponse response = GeneralResponse.builder()
+            .mensaje(mensaje).build();
+    return new ResponseEntity<>(response, HttpStatus.OK);
     }
 @GetMapping("/exponente/{num}/{expo}")
     public double elevarnUmeroAExponente(@PathVariable int num,
---
+++ src/main/java/pe/edu/cibertec/epi_rest_fundamentals/model/GeneralResponse.java
@@ -0,0 +1,10 @@
+package pe.edu.cibertec.epi_rest_fundamentals.model;
+
+import lombok.Builder;
+import lombok.Data;
+@Builder
+@Data
+public class GeneralResponse {
+
+    private String mensaje;
+}
