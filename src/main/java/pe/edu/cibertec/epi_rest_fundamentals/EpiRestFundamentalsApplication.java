c3002b7de20daaf1cbd96fde828b0567a1528b61
goshaaaa777 <isaiasddx322z@gmail.com>
2024-06-18 20:54:26 -0500

    first commit

---
+++ src/main/java/pe/edu/cibertec/epi_rest_fundamentals/EpiRestFundamentalsApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.epi_rest_fundamentals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpiRestFundamentalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpiRestFundamentalsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/epi_rest_fundamentals/EpiRestFundamentalsApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.epi_rest_fundamentals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpiRestFundamentalsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
